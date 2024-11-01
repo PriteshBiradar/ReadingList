1efed239a3f97a6d5ece92e92eb6e18082e6f790
pritesh <priteshrajubiradar@gmail.com>
2024-11-01 11:03:23 +0530

    initial Commit

--- src/main/java/readinglist/SecurityConfig.java
+++ src/main/java/readinglist/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,6 +24,6 @@ public class SecurityConfig {
     }
     @Bean
     public UserDetailsService userDetailsService(){
-        re
+        return new InMemoryUserDetailsManager();
     }
 }
