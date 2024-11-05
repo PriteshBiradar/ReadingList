ae59803e1855e6db25bd47f5e631cd4525801832
pritesh <priteshrajubiradar@gmail.com>
2024-11-05 21:29:10 +0530

    SuccessFully add login page and error page.

--- src/main/java/readinglist/SecurityConfig.java
+++ src/main/java/readinglist/SecurityConfig.java
@@ -3,33 +3,71 @@ package readinglist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import readinglist.readerrepo.Reader;
 import readinglist.readerrepo.ReaderRepository;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
     private ReaderRepository readerrepository;
+
     @Bean
-    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-        http.csrf(Customizer -> Customizer.disable());
-        http.authorizeHttpRequests(request-> request.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        http.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                        .maximumSessions(1)
+                        .expiredUrl("/login?expired=true")
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/css/**", "/js/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .formLogin
+                        (form -> form
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/readinglist.html", true)
+                                .failureUrl("/error")
+                                .permitAll()
+                        )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                        .deleteCookies("JSESSIONID")
+                        .invalidateHttpSession(true)
+                        .permitAll()
+                );
         return http.build();
     }
 
-    protected void Configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(username -> readerrepository.findByUsername(username));
-    }
-
-
 }
+
+
--- src/main/java/readinglist/readerrepo/ReaderRepository.java
+++ src/main/java/readinglist/readerrepo/ReaderRepository.java
@@ -1,6 +1,9 @@
 package readinglist.readerrepo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 public interface ReaderRepository extends  JpaRepository<Reader,String >{
-    Reader findByUsername(String username);
+    Optional<Reader> findByUsername(String username);
 }
